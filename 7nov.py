d3344dab49138f94a260cf1e56e73a381abe5e57
é o gordo <gabrielsouza152007@gmail.com>
2024-11-07 21:00:05 -0300

    Add files via upload

---
+++ 7nov.py
@@ -0,0 +1,29 @@
+
+#exemplo 1
+numero= 10 
+if numero == 10: 
+    print("O número é igual a 10.")
+
+#exemplo 2
+
+idade = 18
+if idade< 18:
+    print('Você é menor de idade')
+elif idade == 18:
+    print('Você acabou de se tornar um adulto')
+else:
+    print('Você é maior de idade')
+
+#Exemplo 3
+x = 10
+if x > 0 and x < 20:
+    print("x está entre 0 e 20")
+
+#Exemplo 4
+
+y = 30
+if y == 25 or y == 50:
+    print("y é 25 ou 50")
+#Exemplo 5
+if (x > 10 and x < 20) or (x == 25):
+    print("x está em um intervalo interessante")
